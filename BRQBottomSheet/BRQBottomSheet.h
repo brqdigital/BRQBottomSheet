72910b2a8a3349468ced8e36ecf60d4065d5ff20
Bruno Faganello <31412688@mackenzista.com.br>
2019-07-17 14:25:30 -0300

    Initial project setup

---
+++ BRQBottomSheet/BRQBottomSheet.h
@@ -0,0 +1,19 @@
+//
+//  BRQBottomSheet.h
+//  BRQBottomSheet
+//
+//  Created by Bruno Faganello Neto on 17/07/19.
+//  Copyright © 2019 Faganello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BRQBottomSheet.
+FOUNDATION_EXPORT double BRQBottomSheetVersionNumber;
+
+//! Project version string for BRQBottomSheet.
+FOUNDATION_EXPORT const unsigned char BRQBottomSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BRQBottomSheet/PublicHeader.h>
+
+
---
+++ BRQBottomSheetExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRQBottomSheetExample
+//
+//  Created by Bruno Faganello Neto on 17/07/19.
+//  Copyright © 2019 Faganello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
